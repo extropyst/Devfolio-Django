ee78c25cf031fb9b626a9a3d36ee4d2cf1a17f4f
extropista <77421034+extropista@users.noreply.github.com>
2023-01-31 03:51:16 +0000

    database mysql a settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,14 @@ DATABASES ={
         }
     }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        },
+    }
+}
 
 
 
